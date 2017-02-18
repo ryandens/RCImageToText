b25c46104cb9f43eb5fa0cd07d17bd3cb6acc17c
Sanat Deshpande <sanatdeshpandejhu@gmail.com>
2017-02-17 22:18:36 -0500

    Parts of the text recongnizer and camera are set up

--- ocr-reader-start/app/src/main/java/com/google/android/gms/samples/vision/ocrreader/OcrCaptureActivity.java
+++ ocr-reader-start/app/src/main/java/com/google/android/gms/samples/vision/ocrreader/OcrCaptureActivity.java
@@ -170,11 +170,34 @@ public final class OcrCaptureActivity extends AppCompatActivity {
         Context context = getApplicationContext();
 
         // TODO: Create the TextRecognizer
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(context).build();
         // TODO: Set the TextRecognizer's Processor.
 
         // TODO: Check if the TextRecognizer is operational.
+        if (!textRecognizer.isOperational()) {
+            Log.w(TAG, "Detector dependencies are not yet available.");
+
+            // Check for low storage.  If there is low storage, the native library will not be
+            // downloaded, so detection will not become operational.
+            IntentFilter lowstorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
+            boolean hasLowStorage = registerReceiver(null, lowstorageFilter) != null;
+
+            if (hasLowStorage) {
+                Toast.makeText(this, R.string.low_storage_error, Toast.LENGTH_LONG).show();
+                Log.w(TAG, getString(R.string.low_storage_error));
+            }
+        }
+
 
         // TODO: Create the mCameraSource using the TextRecognizer.
+        mCameraSource =
+                new CameraSource.Builder(getApplicationContext(), textRecognizer)
+                        .setFacing(CameraSource.CAMERA_FACING_BACK)
+                        .setRequestedPreviewSize(1280, 1024)
+                        .setRequestedFps(15.0f)
+                        .setFlashMode(useFlash ? Camera.Parameters.FLASH_MODE_TORCH : null)
+                        .setFocusMode(autoFocus ? Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE : null)
+                        .build();
     }
 
     /**
